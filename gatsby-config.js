ff3ca96c9ec76500341b7fe2a14032b6712b83fc
Xuanyu Wang <Xuanyu.Wang@ibm.com>
2020-10-06 20:09:44 -0400

    Add the title of home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,20 @@ module.exports = {
   siteMetadata: {
   },
   plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/data/homePageData`,
+        name: 'HomePageData'
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/data/labData`,
+        name: 'LabData'
+      }
+    }
   ],
 }
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import PageHelmet from '../PageHelmet/PageHelmet';
+
+const Title = () => {
+	return <Container style={{ position: 'fixed', top: '50%', left: 0, transform: 'translateY(-50%)' }}>
+		<Typography variant='h4' align='center'>You can think me as a</Typography>
+		<Typography variant='h2' align='center'>front-end developer</Typography>
+	</Container>
+};
+const HomePage = () => {
+	return <Container>
+		<PageHelmet/>
+		{Title()}
+	</Container>
+};
+
+export default HomePage;
