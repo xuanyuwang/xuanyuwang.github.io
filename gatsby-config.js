c671af0083f054126a6e0d0030e77ffda85ac5ff
Xuanyu Wang <Xuanyu.Wang@ibm.com>
2020-10-10 14:17:16 -0400

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,20 +3,6 @@ module.exports = {
   siteMetadata: {
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/data/homePageData`,
-        name: 'HomePageData'
-      }
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/data/labData`,
-        name: 'LabData'
-      }
-    }
+    'gatsby-plugin-react-helmet'
   ],
 }
