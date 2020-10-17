46c823d8409d629779a917f917b3a4070f91341a
Xuanyu Wang <Xuanyu.Wang@ibm.com>
2020-10-16 20:26:18 -0400

    remove maxWidth limit of home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,13 +4,13 @@ import Typography from '@material-ui/core/Typography';
 import PageHelmet from '../PageHelmet/PageHelmet';
 
 const Title = () => {
-	return <Container style={{ position: 'fixed', top: '50%', left: 0, transform: 'translateY(-50%)' }}>
+	return <Container maxWidth={false} style={{ position: 'fixed', top: '50%', left: 0, transform: 'translateY(-50%)' }}>
 		<Typography variant='h4' align='center'>You can think me as a</Typography>
 		<Typography variant='h2' align='center'>front-end developer</Typography>
 	</Container>
 };
 const HomePage = () => {
-	return <Container>
+	return <Container maxWidth={false}>
 		<PageHelmet/>
 		{Title()}
 	</Container>
