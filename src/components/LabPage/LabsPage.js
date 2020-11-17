acc69e128a5445eda35bf1765ca102c873c877f9
Xuanyu Wang <Xuanyu.Wang@ibm.com>
2020-11-17 00:09:47 -0500

    add lab page

--- src/components/LabPage/LabsPage.js
+++ src/components/LabPage/LabsPage.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 import PageHelmet from '../PageHelmet/PageHelmet';
-import Earth from './Labs/Earth/Earth';
+import { useStaticQuery } from 'gatsby';
 
-const HomePage = () => {
+const LabsHomePage = () => {
+	const data = useStaticQuery(graphql`
+    {
+      allPrismicLab {
+        edges {
+          node {
+            data {
+			  name
+			  path
+              date(formatString: "YYYY MMM DD")
+            }
+          }
+        }
+      }
+	}`);
+	const labs = data.allPrismicLab.edges.map((edge) => edge.node.data);
 	return <Container>
 		<PageHelmet/>
-		<Earth/>
+		<GridList>
+			{labs.map((lab) => {
+				return <GridListTile>
+						<Card >
+							<CardContent>
+								<Typography>
+									<Link href={`${lab.path}`}>
+										{lab.name}
+									</Link>
+								</Typography>
+							</CardContent>
+						</Card>;
+				</GridListTile>;
+			})}
+		</GridList>
 	</Container>
 };
 
-export default HomePage;
+export default LabsHomePage;
