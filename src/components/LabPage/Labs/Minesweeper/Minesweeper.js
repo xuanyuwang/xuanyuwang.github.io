fb163134a30e70c4c19cc48f0f665c6e1a2987ef
Xuanyu Wang <Xuanyu.Wang@ibm.com>
2020-11-22 20:26:50 -0500

    use Redux + React

--- src/components/LabPage/Labs/Minesweeper/Minesweeper.js
+++ src/components/LabPage/Labs/Minesweeper/Minesweeper.js
@@ -1,109 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Container from '@material-ui/core/Container';
 import Tooltip from '@material-ui/core/Tooltip';
 
+import { connect, Provider } from 'react-redux';
+import MineSweeperStore from './Minesweeper-redux';
+import { CELL_STATUS, RevealCell, FlagCell } from './Minesweeper-redux';
+
 import './Minesweeper.scss';
 
-const BOARD_SIZES = {
-	SMALL: 'small',
-	MEDIUM: 'meidum',
-	LARGE: 'large'
-};
-const getBoardSpec = (size) => {
-	const specs = {
-		[BOARD_SIZES.SMALL]: {
-			row: 9,
-			col: 9,
-			mines: 10
-		},
-		[BOARD_SIZES.MEDIUM]: {
-			row: 16,
-			col: 16,
-			mines: 40
-		},
-		[BOARD_SIZES.LARGE]: {
-			row: 16,
-			col: 30,
-			mines: 99
-		}
-	};
-	return size && BOARD_SIZES[size] ? specs[size] : specs[BOARD_SIZES.SMALL];
-};
-
-const GAME_STATUS = {
-	INIT: 'initial',
-	PLAYING: 'playing',
-	WIN: 'win',
-	FAIL: 'fail'
-};
-
-const CELL_STATUS = {
-	COVERED: 'covered',
-	FLAGGED: 'flagged',
-	UNCOVERED: 'uncovered'
-};
-
-const ACTIONS = {
-	LEFT_CLICK: 'left-click',
-	RIGHT_CLICK: 'right-click'
-};
-function locateMines(size) {
-	const { row, col, mines } = size;
-	const mineIndices = [];
-	let cellIndices = [];
-	for (let i = 0; i < row * col; i++) {
-		cellIndices.push(i);
-	}
-	while (mineIndices.length < mines) {
-		const index = Math.floor(Math.random() * cellIndices.length);
-		const cellOrder = cellIndices[index];
-		mineIndices.push({ row: Math.floor(cellOrder / col), col: cellOrder % row, order: cellOrder });
-		cellIndices.splice(index, 1);
-	}
-	return mineIndices;
-};
-
-const useCellsInfo = (mineIndices, boardRow, boardCol) => {
-	const mineCount = [];
-	for (let i = 0; i < boardRow * boardCol; i++) {
-		mineCount.push({
-			surrondingMines: 0,
-			displayValue: '',
-			status: CELL_STATUS.COVERED
-		});
-	}
-	mineIndices.forEach((mineIndex) => {
-		const { row: mineRow, col: mineCol } = mineIndex;
-		const neighbors = [
-			[mineRow + 1, mineCol], // above
-			[mineRow - 1, mineCol], // below
-			[mineRow, mineCol + 1], // right
-			[mineRow, mineCol - 1], // left
-			[mineRow - 1, mineCol - 1], // top left
-			[mineRow - 1, mineCol + 1], // top right
-			[mineRow + 1, mineCol - 1], // bottom left
-			[mineRow + 1, mineCol + 1] // bottom right
-		];
-		neighbors.forEach((neighbor) => {
-			if (neighbor[0] >= 0 &&
-				neighbor[0] < boardRow &&
-				neighbor[1] >= 0 &&
-				neighbor[1] < boardCol
-			) {
-				const neighborIndex = neighbor[0] * boardCol + neighbor[1];
-				mineCount[neighborIndex].surrondingMines += 1;
-			}
-		});
-	});
-	mineIndices.forEach((mineIndex) => {
-		const { row: mineRow, col: mineCol } = mineIndex;
-		mineCount[mineRow * boardCol + mineCol].surrondingMines = -1;
-	})
-
-	return mineCount;
-};
-const StatusBar = (props) => {
-	const { flags, gameStatus } = props;
+const ConnectedStatusBar = ({ GameStatus }) => {
 	return <Container className='status-bar'>
 		<div className='flag'>
 			{/* <Container className='flag-content'>
@@ -112,27 +17,36 @@ const StatusBar = (props) => {
 			</Container> */}
 		</div>
 		<Container className='game-status'>
-			{gameStatus}
+			{GameStatus}
 		</Container>
 	</Container>;
 };
+const StatusBar = connect((state) => {
+	return { ...state };
+})(ConnectedStatusBar);
+
 const Cell = (props) => {
 	const {
-		row,
-		col,
-		value,
-		isMine,
+		cell,
+		RevealCell,
+		FlagCell,
 		cellSize,
-		status,
-		onAction
 	} = props;
+	const {
+		surroundingMines,
+		cellRow: row,
+		cellCol: col,
+		status,
+		displayValue: value
+	} = cell;
 	let className = `cell ${(row + col) % 2 === 0 ? 'odd' : 'even'}`;
 	className += status === CELL_STATUS.COVERED ? '' : ' touched';
-	const cell = <div
+	const element = <div
 		row={row + 1}
 		col={col + 1}
-		ismine={isMine ? 'true' : 'false'}
 		key={`${row} ${col}`}
+		ismine={surroundingMines === -1 ? 'true' : 'false'}
+		surroundingmines={surroundingMines}
 		className={className}
 		style={{
 			gridColumn: col + 1,
@@ -142,133 +56,35 @@ const Cell = (props) => {
 		}}
 		onClick={(event) => {
 			event.preventDefault();
-			onAction(row, col, ACTIONS.LEFT_CLICK);
+			RevealCell(cell);
 		}}
 		onContextMenu={(event) => {
 			event.preventDefault();
-			onAction(row, col, ACTIONS.RIGHT_CLICK);
+			FlagCell(cell);
 		}}
 	>
 		<div>
 			{value}
 		</div>
 	</div>;
-	return <div>{cell}</div>;
-	// return <Tooltip title={isMine ? 'true' : 'false'} key={`${row} ${col}`}>
-	// 	{cell}
+	return <div>{element}</div>;
+	// return <Tooltip title={ surroundingMines === -1 ? 'true' : 'false'} key={`${row} ${col}`}>
+	// 	{element}
 	// </Tooltip>;
 };
-const Board = (props) => {
-	const { size, gameStatus, setGameStatus } = props;
-	const { row, col, mines } = size;
-
-	const [mineIndices] = useState(locateMines(size));
-	// { surrondingMines, displayValue, status }
-	const [cellsInfo, setCellsInfo] = useState(useCellsInfo(mineIndices, row, col));
-
-	const [markedMines, setMarkedMines] = useState(0);
-	const [remainCells, setRemainCells] = useState(row * col);
-	const [stepOnMine, setStepOnMine] = useState(false);
-
-	useEffect(() => {
-		if (stepOnMine) {
-			if (gameStatus !== GAME_STATUS.FAIL) {
-				setGameStatus(GAME_STATUS.FAIL);
-			}
-		} else if (markedMines === mines && remainCells === 0) {
-			if (gameStatus !== GAME_STATUS.WIN) {
-				setGameStatus(GAME_STATUS.WIN);
-			}
-		} else if (remainCells < row * col && !stepOnMine) {
-			if (gameStatus !== GAME_STATUS.PLAYING) {
-				setGameStatus(GAME_STATUS.PLAYING);
-			}
-		} else {
-			if (gameStatus !== GAME_STATUS.INIT) {
-				setGameStatus(GAME_STATUS.INIT);
-			}
-		}
+const ConnectedBoard = (props) => {
+	const { BoardRow, BoardCol, Cells, RevealCell, FlagCell } = props;
+	const cellSize = 32;
+	const cells = Cells.map((cell) => {
+		return Cell({ cellSize: 32, RevealCell, FlagCell, cell });
 	});
 
-	const onAction = (cellRow, cellCol, action) => {
-		if (gameStatus === GAME_STATUS.FAIL || gameStatus === GAME_STATUS.WIN) {
-			return;
-		}
-		const cellOrder = cellRow * row + cellCol;
-		const cellInfo = cellsInfo[cellOrder];
-		if (action === ACTIONS.LEFT_CLICK) {
-			if (cellInfo.status === CELL_STATUS.COVERED) {
-				const newCellInfo = {};
-				if (cellInfo.surrondingMines === -1) {
-					newCellInfo.displayValue = 'm';
-				} else {
-					if (cellInfo.surrondingMines === 0) {
-						cellInfo.displayValue = '';
-					} else {
-						cellInfo.displayValue = cellInfo.surrondingMines;
-					}
-				}
-				newCellInfo.status = CELL_STATUS.UNCOVERED;
-
-				setCellsInfo(() => {
-					Object.assign(cellsInfo[cellOrder], newCellInfo);
-					return cellsInfo;
-				});
-
-				setRemainCells(() => {
-					return remainCells - 1;
-				});
-
-				if (newCellInfo.displayValue === 'm') {
-					setStepOnMine(true);
-				}
-			}
-		} else if (action === ACTIONS.RIGHT_CLICK) {
-			if (cellInfo.status === CELL_STATUS.COVERED) {
-				const newCellInfo = {};
-				newCellInfo.displayValue = 'f';
-				newCellInfo.status = CELL_STATUS.FLAGGED;
-
-				setCellsInfo(() => {
-					Object.assign(cellsInfo[cellOrder], newCellInfo);
-					return cellsInfo;
-				});
-
-				setRemainCells(() => {
-					return remainCells - 1;
-				});
-
-				if (cellInfo.surrondingMines === -1) {
-					setMarkedMines(() => {
-						return markedMines - 1;
-					});
-				}
-			}
-		}
-	};
-
-	const cells = [];
-	const cellSize = 32;
-	for (let i = 0; i < row; i++) {
-		for (let j = 0; j < col; j++) {
-			const cellInfo = cellsInfo[i * row + j];
-			cells.push(Cell({
-				row: i,
-				col: j,
-				value: cellInfo.displayValue,
-				isMine: cellInfo.surrondingMines === -1,
-				cellSize,
-				status: cellInfo.status,
-				onAction
-			}));
-		}
-	}
 	return <div>
 		<Container className='board' style={{
-			width: `${col * cellSize}px`,
-			height: `${row * cellSize}px`,
-			gridTemplateColumns: `repeat(${col}, ${cellSize}px)`,
-			gridTemplateRows: `repeat(${row} ${cellSize}px)`
+			width: `${BoardCol * cellSize}px`,
+			height: `${BoardRow * cellSize}px`,
+			gridTemplateColumns: `repeat(${BoardCol}, ${cellSize}px)`,
+			gridTemplateRows: `repeat(${BoardRow} ${cellSize}px)`
 		}}>
 			{cells}
 		</Container>
@@ -277,21 +93,20 @@ const Board = (props) => {
 		}</div> */}
 	</div>;
 };
-const Game = () => {
-	const [size, setSize] = useState(getBoardSpec());
-	const [gameStatus, setGameStatus] = useState(GAME_STATUS.INIT);
-	return <Container
-		className='game'
-	>
-		{StatusBar({ gameStatus })}
-		{Board({ size, gameStatus, setGameStatus })}
-	</Container>;
-};
+const Board = connect((state) => {
+	return { ...state };
+}, {
+	RevealCell,
+	FlagCell
+})(ConnectedBoard);
+
 const MineSweeper = () => {
-	const game = Game();
-	return <Container className='minesweeper'>
-		{game}
-	</Container>;
+	return <Provider store={MineSweeperStore}>
+		<Container className='minesweeper'>
+			<StatusBar/>
+			<Board/>
+		</Container>
+	</Provider>;
 };
 
 export default MineSweeper;
