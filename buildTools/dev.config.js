4685901faee9a65b55e15becfd5e49c625867e1f
Xuanyu Wang <xuanyu.wang@outlook.com>
2021-02-26 21:16:59 -0500

    remove hand written html

--- buildTools/dev.config.js
+++ buildTools/dev.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: '../src/js/entrypoint/index.js',
+    entry: {
+        index: './src/js/entrypoint/index.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "Xuanyu"
+        })
+    ],
     output: {
-        filename: 'index.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, '../dist')
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    mode: "development"
 }
--- src/js/entrypoint/index.js
+++ src/js/entrypoint/index.js
@@ -1,1 +1,4 @@
-import React from react;
+const body = document.body;
+const header = document.createElement('h1');
+header.innerText = "You can think me as a front-end enginner";
+body.appendChild(header);
