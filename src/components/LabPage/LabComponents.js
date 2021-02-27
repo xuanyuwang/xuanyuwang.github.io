0fada24857f1f8a3760ded67531dcf1b7a9f3fce
Xuanyu Wang <xuanyu.wang@outlook.com>
2021-02-26 20:14:56 -0500

    clean and add react dependencies

--- src/components/LabPage/LabComponents.js
+++
@@ -1,8 +0,0 @@
-import Earth from './Labs/Earth/Earth';
-import Minesweeper from './Labs/Minesweeper/Minesweeper';
-const componentMap = {
-	'Draggable earth': Earth,
-	'Minesweeper': Minesweeper
-};
-
-export default componentMap;
--- src/pages/labs.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LabsPage from '../components/LabPage/LabsPage';
-
-const Labs = () => {
-	return <LabsPage/>;
-};
-
-export default Labs;
