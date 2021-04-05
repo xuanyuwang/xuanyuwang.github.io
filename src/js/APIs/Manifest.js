78e5d89954e9e9594e702240ac421f0eb09e3bdc
Xuanyu Wang <xuanyu.wang@outlook.com>
2021-04-04 22:15:17 -0400

    1. set About Me page at the default page 2. embed resume as pdf

--- src/js/APIs/Manifest.js
+++ src/js/APIs/Manifest.js
@@ -3,7 +3,7 @@ const PageNames = {
 	ME: "ME"
 };
 const Manifest = {
-	"StarterPage": PageNames.HOME,
+	"StarterPage": PageNames.ME,
 	"Pages": {
 		[PageNames.HOME]: 'js/pages/HomePage',
 		[PageNames.ME]: 'js/pages/Me'
--- src/js/pages/Me.jsx
+++ src/js/pages/Me.jsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
+import { Link } from 'carbon-components-react';
+import resume from 'static/Resume-Xuanyu-Wang.pdf';
+import './Me.scss';
 
-const HomePage = () => {
-	return <div>
-        This is about me
+const pageIDPrefix = 'page-me';
+
+const Page = () => {
+	return <div id={pageIDPrefix}>
+		<object id={`${pageIDPrefix}-pdf-containe`} data={resume} type={"application/pdf"}>
+			<div>
+				Your browser does not support viewing PDF. Please use this <Link href="https://www.linkedin.com/in/xuanyu-wang/">My LinkedIn profile</Link> instead.
+			</div>
+		</object>
 	</div>;
 };
 
-export default HomePage;
+export default Page;
