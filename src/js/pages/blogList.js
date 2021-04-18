82e52716e20e13b2956656f713a93e91483fe342
Xuanyu Wang <xuanyu.wang@outlook.com>
2021-04-18 15:14:16 -0400

    Drag & drop: mouse events, vanilla JavaScript, and only one object

--- src/js/pages/blogList.js
+++ src/js/pages/blogList.js
@@ -2,6 +2,10 @@ const List = [
 	{
 		name: 'The structure of a web project',
 		link: 'https://www.notion.so/Web-Project-28adcdb3b74344d8b87764e00991e6db'
+	},
+	{
+		name: 'Drag & drop: mouse events, vanilla JavaScript, and only one object',
+		link: 'https://www.notion.so/Drag-drop-mouse-events-vanilla-JavaScript-and-only-one-object-0bdcf3d0799e44cfa399fbc6d56839bf'
 	}
 ];
 
